452d9044f98570404810ab51abf9e56c61c14089
nvsingh2001 <nvsingh2001@hotmail.com>
2025-12-01 16:56:20 +0530

    feat: Added a new Line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void name() {
         System.out.println("Hello World!");
         System.out.println("My name is Naman!");
+        System.out.println("Added A new Line!");
     }
 }
